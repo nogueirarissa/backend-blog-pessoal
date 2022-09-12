53f55b5944897a54575d7662d9082b063cdcef43
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-12 11:26:03 -0300

    Atualiza Requisição por descrição

--- src/main/java/org/generation/blogPessoal/controller/TemaController.java
+++ src/main/java/org/generation/blogPessoal/controller/TemaController.java
@@ -36,7 +36,7 @@ public class TemaController {
 	}
 	
 	@GetMapping("/descricao/{descricao}")
-	public ResponseEntity<List<Tema>> getByTitle(@PathVariable String descricao){
+	public ResponseEntity<List<Tema>> getByDescricao(@PathVariable String descricao){
 		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
 	}
 	
