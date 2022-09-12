f319961587ea319c8263616690aff26c36f99c50
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-12 12:00:12 -0300

    Atualiza UsuarioRepository

--- src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -1,10 +1,11 @@
 package org.generation.blogPessoal.repository;
 
-import java.util.List;
+
+import java.util.Optional;
 
 import org.generation.blogPessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
-	public List<Usuario> findByUsuario(String usuario);
+	public Optional<Usuario> findByUsuario(String usuario);
 }
