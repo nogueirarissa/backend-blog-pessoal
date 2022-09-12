904d210fb2623466a649c55d1c50603ecd20e5d4
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-12 11:43:43 -0300

    Cria relacionamento One2Many com a Classe postagem

--- src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,11 +1,17 @@
 package org.generation.blogPessoal.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -25,6 +31,18 @@ public class Usuario {
 	private String senha;
 	
 	private String foto;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 
 	public Long getId() {
 		return id;
