08f3b1e1955d44176bda6f3f06934d526ca51f72
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-15 14:24:56 -0300

    Cria métodos construtores

--- src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -38,6 +38,17 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
+	
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	public Usuario() {}
+	
 	public List<Postagem> getPostagem() {
 		return postagem;
 	}
